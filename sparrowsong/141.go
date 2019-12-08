419c4c429d5e52c4eb56bca93500b06fe6fa9c76
sparrowsong <sparrowsong@tencent.com>
2019-12-08 23:36:00 +0800

    xx

---
+++ sparrowsong/141.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func hasCycle(head *ListNode) bool {
+	fast := head
+	slow := head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
