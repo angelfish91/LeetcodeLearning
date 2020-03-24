e0aecdc178cdd00b7cd9fb452dbf8fa43c532321
Lzq <lzqlys@126.com>
2020-03-24 10:04:06 +0800

    对876题链表的中间节点进行补充

--- LIUzq/876.cpp
+++ LIUzq/876.cpp
@@ -1,4 +1,5 @@
 链表的中间节点
+【本题中的含有头结点实际上是错误的】头结点怎么能有val呢，应该是null才对
 
 /**
  * Definition for singly-linked list.
@@ -25,5 +26,28 @@ public:
             cur = cur->next;
         }
         return cur;
+    }
+};
+
+//快慢指针：
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode * p = head;
+        ListNode * q = head;
+        while(q != NULL && q->next != NULL){    //p != NULL不是这个，而是q，p肯定不会为空
+            p = p->next;
+            q = q->next->next;
+        }
+        return p;
     }
 };
