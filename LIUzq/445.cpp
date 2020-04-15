2468c5c5c3fc714aca0754bb5aa612ca2a0cc02f
Lzq <lzqlys@126.com>
2020-04-15 09:12:30 +0800

    445增加官方解答，不用vector只用链表就倒着（按书写习惯）保存数据，不需要reverse

--- LIUzq/445.cpp
+++ LIUzq/445.cpp
@@ -62,3 +62,43 @@ public:
         return head;
     }
 };
+
+//官方解答
+//没有用到vector
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        stack<int> s1, s2;
+        while (l1) {
+            s1.push(l1 -> val);
+            l1 = l1 -> next;
+        }
+        while (l2) {
+            s2.push(l2 -> val);
+            l2 = l2 -> next;
+        }
+        int carry = 0;
+        ListNode* ans = nullptr;
+        while (!s1.empty() or !s2.empty() or carry != 0) {
+            int a = s1.empty() ? 0 : s1.top();
+            int b = s2.empty() ? 0 : s2.top();
+            if (!s1.empty()) s1.pop();
+            if (!s2.empty()) s2.pop();
+            int cur = a + b + carry;
+            carry = cur / 10;
+            cur %= 10;
+			
+			//这部分！！！
+			//【特别巧妙】链表倒着保存了，按书写习惯保存，不需要再reverse，【画图便知】
+            auto curnode = new ListNode(cur);
+            curnode -> next = ans;
+            ans = curnode;
+        }
+        return ans;
+    }
+};
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/add-two-numbers-ii/solution/liang-shu-xiang-jia-ii-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
